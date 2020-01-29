946d47462aaf3a52624e5f21362758ee7e4e1caa
Barbara McGillivray <bmcgillivray@turing.ac.uk>
2020-01-29 13:10:23 +0000

    Update interannotator_LatinISE.py

--- interannotator_LatinISE.py
+++ interannotator_LatinISE.py
@@ -21,6 +21,10 @@ import re
 from collections import Counter
 import locale
 from pandas import read_excel
+import numpy as np
+import math
+from statistics import mean
+from scipy.stats import spearmanr
 
 
 now = datetime.datetime.now()
@@ -32,13 +36,13 @@ istest_default = "yes"
 istest = input("Is this a test? Leave empty for default (" + istest_default + ").")
 number_test = 2 # number of annotators considered when testing
 
-annotators = ["Annie", "Daria",  "Hugo", "Rozi"]#, "Hege"]
+annotators = ["Annie", "Daria",  "Hugo", "Rozi", "Hege"]
 
 if istest == "":
     istest = istest_default
 
 if istest == "yes":
-    annotators = annotators[1:2]
+    annotators = annotators[:2]
 
 # Directory and file names:
 
@@ -48,30 +52,81 @@ directory = os.path.join("/Users", "bmcgillivray", "Documents", "OneDrive", "The
 dir_annotation = os.path.join(directory, "Semantic annotation", "Annotated data", "selected", "target words", "virtus")
 
 # Output file:
-agreement_file_name = "inter-annotator.csv"
+agreement_file_name = "inter-annotator.txt"
 
 if istest == "yes":
-    agreement_file_name = agreement_file_name.replace(".csv", "_test.csv")
+    agreement_file_name = agreement_file_name.replace(".txt", "_test.txt")
 
+
+# This function normalizes the annotators' ratings, because sometimes they marked a number (e.g. "1")
+#  and sometimes a string (e.g. "1: Identical":
+
+def normalize_ratings(ratings):
+    #print("shape:", str(ratings.shape))
+    if len(str(ratings.iloc[1,3])) > 1:
+        for column in range(ratings.shape[1]):
+            #print("column", str(column))
+            #print("old:\n", ratings.iloc[column])
+            ratings.iloc[:,column] = ratings.iloc[:,column].str.split(': ').str[0]
+            #print("new:", ratings.iloc[column])
+    #print(str(ratings))
+    return ratings
+
+output = open(os.path.join(dir_annotation, agreement_file_name), 'w')
 
 # Read annotation files:
 
 for annotator1 in annotators:
     for annotator2 in annotators:
-        if annotator1 != annotator2:
-
-            annotated_file1 = open(os.path.join(dir_annotation, "Annotation_task1_virtus_" + annotator1 + ".xlsx"), 'r', encoding="utf-8")
-            annotated_file2 = open(os.path.join(dir_annotation, "Annotation_task2_virtus_" + annotator2 + ".xlsx"), 'r',
-                                   encoding="utf-8")
+        if annotator1 < annotator2:
 
             my_sheet = 'Annotation'
-            ann1 = read_excel(annotated_file1, sheet_name=my_sheet)
-            ann2 = read_excel(annotated_file2, sheet_name=my_sheet)
-            print(ann1.head())  # shows headers with top 5 rows
-            print(str(ann1.shape))
-            print(ann2.head())  # shows headers with top 5 rows
-            print(str(ann2.shape))
+            ann1 = read_excel(os.path.join(dir_annotation, "Annotation_task1_virtus_" + annotator1 + ".xlsx"),
+                              sheet_name=my_sheet, encoding='utf-8')
+            ann2 = read_excel(os.path.join(dir_annotation, "Annotation_task1_virtus_" + annotator2 + ".xlsx"),
+                              sheet_name=my_sheet)
 
-            annotated_file1.close()
-            annotated_file2.close()
+            print("First annotator:" + annotator1)
+            print(str(ann1.shape[0]), "rows", ann1.shape[1], "columns")
 
+            ratings1 = ann1.iloc[0:61, 3:ann1.shape[1]-1]
+            #print(str(ratings1))
+            normalize_ratings(ratings1)
+            #print(str(ratings1))
+
+            print("Second annotator:" + annotator2)
+            print(str(ann2.shape[0]), "rows", ann2.shape[1], "columns")
+
+            ratings2 = ann2.iloc[0:61, 3:ann2.shape[1] - 1]
+            normalize_ratings(ratings2)
+            #print(str(ratings2))
+
+
+            # Calculate inter-annotator agreement with Spearman's rho between
+            # first row of the annotated data matrix for annotator 1 and first row of the annotated data matrix for annotator 2;
+            # second row in annotator 1 and second row in annotator 2;
+            # and so on
+
+            rhos = [] # list of Spearman who coefficients, one for each row of the annotated data matrix
+            pvalues = [] # list of p values of Spearman correlation test, one for each row of the annotated data matrix
+
+            for row_n in range(ratings1.shape[0]):
+                #print("Row:", str(row_n))
+                try:
+                    rho, pval = spearmanr(ratings1.iloc[row_n,], ratings2.iloc[row_n,])
+                    if pval >= 0.05:
+                        rho = 'non-sign'
+                    rhos.append(rho)
+                    pvalues.append(pval)
+                except:
+                    continue
+                #print("Calculating spearman rho between ", str(ratings1.iloc[row_n,]), "and", str(ratings2.iloc[row_n,]))
+                #print("rho:", str(rho))
+                #print("pval:", str(pval))
+
+            #print(str(rhos))
+            rhos_sign = [rho for rho in rhos if rho != "non-sign" and not math.isnan(rho) ]
+            print(str(round(mean(rhos_sign),2)))
+            output.write("Mean of Spearman rho betweeen "+ annotator1 + " and " + annotator2 + ": " + str(round(mean(rhos_sign),2)) + "\n")
+
+output.close()
