bc85e28b8b6e395c2009492e2d7a53854ae0b0da
Valentina Lunardi <vlunardi@g.ucla.edu>
2025-04-04 15:14:53 +0100

    restructured folders, added download.sh

---
+++ Christianity_semantic_change/README.md
@@ -0,0 +1,15 @@
+# Christianity-driven semantic change in Latin
+The codes within this folder aim to use different NLP methods – static embeddings, contextual embeddings and collocational analysis – to capture those changes in meaning within the Latin lexicon that were driven by the spread of Christianity among Latin-speaking people. Details about each of these methods/codes can be found below.
+
+## Static embeddings
+
+## Contextual embeddings
+In order to run this code, you will need to have Latin BERT (Bamman and Burns, 2020) installed. Follow [these instructions](https://github.com/dbamman/latin-bert/tree/master).
+
+You will also need to download our own fine-tuned model of Latin BERT, further trained on [LatinISE](https://lindat.mff.cuni.cz/repository/xmlui/handle/11372/LRT-5870) (McGillivray and Kilgarriff, 2013). In order to do so, (1) clone this repo and (2) in your terminal, navigate to this folder and run this in your command line:
+
+```sh
+./contextual_embeddings/download.sh
+```
+
+## Collocational analysis
---
+++ Christianity_semantic_change/contextual_embeddings/download.sh
@@ -0,0 +1,16 @@
+# Download fine-tuned model from Google Drive
+LINK_ID="1KSQzhcxQsk-tWnBBANk-am-YluvypcKD"  # Replace with your file's unique ID
+OUTPUT_FILE="finetuned_latin_bert.tar"             # Replace with your model's file name
+
+# Extract confirmation token and UUID (if required)
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$LINK_ID" -O- | sed -rn 's/.*name="confirm" value="([0-9A-Za-z_]+)".*/\1\n/p')
+UUID=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$LINK_ID" -O- | sed -rn 's/.*name="uuid" value="([0-9A-Za-z_-]+)".*/\1\n/p')
+
+# Download the file
+wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://drive.usercontent.google.com/download?export=download&id=$LINK_ID&confirm=$CONFIRM&uuid=$UUID" -O $OUTPUT_FILE && rm -f /tmp/cookies.txt
+
+# Move and extract the file
+mv $OUTPUT_FILE contextual_embeddings/latin-bert-finetuned/
+cd contextual_embeddings/latin-bert-finetuned
+tar -xf $OUTPUT_FILE
+rm $OUTPUT_FILE
